edca979cb05173b88cba140b3eedd16a100069fe
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-11-13 12:04:33 +0600

    Converting String to Integer Number

--- programming.c
+++ programming.c
@@ -155,13 +155,35 @@
 
 //     puts(str);
 // }
-
 // #include<stdio.h>
 // int main()
 // {
 
-//     char str[] = "Bd is Bea\\ut'y";
+//     char str[] = "5";
 
 
-//     puts(str);
+//     printf("%d", str[0] - '0');
 // }
+
+#include<stdio.h>
+
+int main()
+{
+    char str[20];
+    int n;
+
+    scanf("%d", &n);
+    scanf("%s", str);
+
+    int num = 0;
+    for(int i = 0; i < n; i++){
+
+        int digit = str[i] - '0';
+
+        num = num * 10 + digit;
+    }
+
+    printf("%d\n", num);
+
+    return 0;
+}
